01dc09933fce21a6efc08a7f783d30d6791c38bb
Kofi Dev <emmanuelanaman03@gmail.com>
2026-06-08 16:06:14 +0000

    Add Trusted By section

--- static-saas-landing-page/src/App.jsx
+++ static-saas-landing-page/src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css';
 import { Hero } from './components/Hero/Hero';
 import { Navbar } from './components/NavBar/Navbar';
+import { TrustedBy } from './components/TrustedBy/TrustedBy';
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <main>
+        <TrustedBy />
+      </main>
     </>
   );
 }
---
+++ static-saas-landing-page/src/components/TrustedBy/TrustedBy.jsx
@@ -0,0 +1,26 @@
+import Domain from '../../assets/icons/domain.svg';
+import Business from '../../assets/icons/business.svg';
+import StoreFront from '../../assets/icons/storefront.svg';
+import Apartment from '../../assets/icons/apartment.svg';
+import CorporateFare from '../../assets/icons/corporate_fare.svg';
+import './TrustedBy.css'
+
+export function TrustedBy() {
+  return (
+    <section className='trusted-by'>
+      <h4>TRUSTED BY 10,000+ TEAMS WORLDWIDE</h4>
+
+      <div className="container icon-img">
+        <IconImage src={Domain} alt={Domain} />
+        <IconImage src={Business} alt={Business} />
+        <IconImage src={StoreFront} alt={StoreFront} />
+        <IconImage src={Apartment} alt={Apartment} />
+        <IconImage src={CorporateFare} alt={CorporateFare} />
+      </div>
+    </section>
+  );
+}
+
+function IconImage({ src, alt }) {
+  return <img src={src} alt={alt} className="img-icon" />;
+}
